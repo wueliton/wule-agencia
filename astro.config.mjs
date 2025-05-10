696e8728d7b852994bc9e7ba9f4fb1d0653e1517
Paulo Wueliton <paulowueliton@MacBook-Air-de-Paulo.local>
2025-05-10 19:12:41 -0300

    feat: migrated fonts to local

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 // @ts-ignore
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 // @ts-ignore
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
@@ -15,11 +15,26 @@ export default defineConfig({
   },
   build: {
     assets: "assets",
+    inlineStylesheets: "always",
   },
   output: "static",
   compressHTML: true,
   integrations: [react(), icon(), sitemap()],
   experimental: {
     responsiveImages: true,
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "DM Sans",
+        cssVariable: "--font-secondary",
+        weights: [300, 400, 700],
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Inter",
+        cssVariable: "--font-primary",
+        weights: [300, 400, 500],
+      },
+    ],
   },
 });
